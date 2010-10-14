8ae2d3c5507034539b6d7045bc5f9ef90bcfb909
Leonid Mamchenkov <leonid@mamchenkov.net>
2010-10-14 18:51:05 +0300

    Added sample.php
    The script shows how to lock and unlock process, as well as what
    will happen when trying to lock an already locked process.

---
+++ sample.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once dirname(__FILE__) . '/locker.php';
+
+$lockFile = Locker::getName();
+$lock = Locker::lock($lockFile);
+
+if ($lock) {
+	print "Process is locked now.\n";
+	$sameLock = Locker::lock($lockFile);
+	if ($sameLock) {
+		print "Something is very wrong - locked same process twice.\n";
+	}
+	else {
+		print "Faield to get a second lock on the same process.\n";
+		print "Unlocking\n";
+		$unlock = Locker::unlock($lockFile);
+		if ($unlock) {
+			print "Unlocked\n";
+		}
+		else {
+			print "Failed to unlock\n";
+		}
+	}
+}
+else {
+	print "Failed to lock process.\n";
+}
+
+?>
